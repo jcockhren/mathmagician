9aa5afae46bf9fdc06f382e29cbe73f3a1dd0fd5
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-23 12:35:59 -0700

    added user interaction.

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -10,6 +10,33 @@ namespace Mathmagician
     {
         static void Main(string[] args)
         {
+            string prompt = "> ";
+            Console.WriteLine("What do you want me to do?");
+            Console.Write(prompt);
+            string response = Console.ReadLine();
+            if (response == "natural numbers")
+            {
+                Console.WriteLine("How many?");
+                Console.Write(prompt);
+                string response_length = Console.ReadLine();
+                int length;
+                bool parsed = int.TryParse(response_length, out length);
+                if (parsed)
+                {
+                    NaturalNumbers nats = new NaturalNumbers();
+                    Console.WriteLine(nats.ToString(nats.GetSequence(length)));
+                } else
+                {
+                    Console.WriteLine("Whoops!");
+                }
+                
+            }
+            else {
+                Console.WriteLine("Nope! Do better next time.");
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
         }
     }
 }
