847b42536e268d1d387c4ff49ba2d4b10bd36646
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-23 13:30:30 -0700

    Implementation of even numbers

---
+++ Mathmagician/EvenNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class EvenNumbers : NaturalNumbers
+    {
+        public EvenNumbers()
+        {
+            first = 2;
+        }
+
+        public override int GetNext(int x)
+        {
+            //return x + 2;
+            return base.GetNext(x) + 1;
+        }
+    }
+}
