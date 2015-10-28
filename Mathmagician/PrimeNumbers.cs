1dc84c9cae1f3e6bce1bc4fc5ef53a1015444428
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-28 13:39:37 -0700

    Implemented Prime numbers Class

---
+++ Mathmagician/PrimeNumbers.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class PrimeNumbers : NaturalNumbers
+    {
+        private int[] list_of_primes;
+        private int upper_bound;
+        public PrimeNumbers()
+        {
+            list_of_primes = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
+            first = 2;
+            upper_bound = 200;
+        }
+
+        public bool IsPrime(int x)
+        {
+            bool result = false;
+            bool found_composite = false;
+            int i = 3;
+            while ( i < x)
+            {
+                if (x % i == 0)
+                {
+                  found_composite = true;
+                  break;
+                }
+                i += 2;
+            }
+            if (!found_composite) {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public override int GetNext(int x)
+        {
+            if (x < 100)
+            {
+                return list_of_primes[FindIndex(x) + 1];
+            } else if (x > upper_bound)
+            {
+                throw new ArgumentException("Sowwy. You've exceeded our capabilities.");
+            } else if (!IsPrime(x))
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                int candidate = x+2;
+                while (!IsPrime(candidate)) {
+                    candidate += 2;
+                }
+                return candidate;
+
+            }
+            
+        }
+
+        public int FindIndex(int element) {
+            
+            if (list_of_primes.Contains(element))
+            {
+                for (int i = 0; i < list_of_primes.Length; i++)
+                {
+                    if (list_of_primes[i]== element) {
+                        return i;
+                    }
+                }
+                throw new ArgumentException();
+            } else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+    }
+}
---
+++ MathmagicianTests/PrimeNumberTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mathmagician;
+
+namespace MathmagicianTests
+{
+    [TestClass]
+    public class PrimeNumberTests
+    {
+        [TestMethod]
+        public void PrimeEnsureICanCreateClassInstance()
+        {
+            PrimeNumbers Prime = new PrimeNumbers();
+            Assert.IsNotNull(Prime);
+        }
+
+        [TestMethod]
+        public void PrimeShouldPrintFirstNumber()
+        {
+            PrimeNumbers Prime = new PrimeNumbers();
+            int expected = 2;
+            int actual = Prime.GetFirst();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimeShouldPrintNextNumber()
+        {
+            PrimeNumbers Prime = new PrimeNumbers();
+            int input = 2;
+            int expected = 3;
+            int actual = Prime.GetNext(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimeShouldPrintAVariableSequenceOfNumbers()
+        {
+            PrimeNumbers Prime = new PrimeNumbers();
+            int input = 8;
+            int[] expected = new int[] { 2, 3, 5, 7, 11, 13, 17, 19 };
+            int[] actual = Prime.GetSequence(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimePrintSequenceOfNumbers()
+        {
+            PrimeNumbers Prime = new PrimeNumbers();
+            int length = 7;
+            string expected = "2 3 5 7 11 13 17";
+            string actual = Prime.ToString(Prime.GetSequence(length));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimeFindIndexOfElement()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 73;
+            int expected = 20;
+            int actual = prime.FindIndex(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimeIsPrime()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 73;
+            bool expected = true;
+            bool actual = prime.IsPrime(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PrimeIsNotPrime()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 20;
+            bool expected = false;
+            bool actual = prime.IsPrime(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PrimeFindIndexOfElementFails()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 103;
+            prime.FindIndex(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PrimeGetNextNonPrime()
+        {
+            PrimeNumbers prime = new PrimeNumbers();
+            int input = 105;
+            prime.GetNext(input);
+        }
+    }
+}
