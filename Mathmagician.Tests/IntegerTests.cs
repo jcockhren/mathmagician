b0bba205c3963f9ba03d3634c32cfd3f4557adea
Jurnell Cockhren <jurnell@sophicware.com>
2016-02-22 18:49:15 -0800

    Finished tests for Integer Class

---
+++ Mathmagician.Tests/IntegerTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class IntegerTests
+    {
+        [TestMethod]
+        public void IntegerEnsureICanCreateAnInstance()
+        {
+            Integer my_int = new Integer();
+            Assert.IsNotNull(my_int);
+        }
+
+        [TestMethod]
+        public void IntegerEnsureICanGetFirstNumber()
+        {
+            // Arrange - Scenario Setup
+            Integer my_int = new Integer();
+
+            // Act - Do the thing you want to test
+            int actual = my_int.GetFirst();
+            int expected = 0;
+
+            // Assert - Did it work as expected?
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IntegerEnsureICanGetNextInteger()
+        {
+            // Arrange
+            Integer my_int = new Integer();
+
+            // Act
+
+            int actual = my_int.GetNext(5);
+            int expected = 6;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IntegerEnsureICanCreateASequenceOfTenIntegers()
+        {
+            // Arrange
+            Integer my_int = new Integer();
+
+            // Act
+            int[] actual = my_int.GetSequence(10);
+            int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void IntegerEnsureMaxIsNotExceeded()
+        {
+            // Arrange
+            Integer my_int = new Integer();
+            // Act
+            int[] actual = my_int.GetSequence(my_int.Max+1);
+            // This is a call the uses an older testing framework. (e.g. Koans)
+            //Assert.Throws(typeof(Exception), my_int.GetSequence(my_int.Max));
+        }
+    }
+}
---
+++ Mathmagician/Integer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Integer
+    {
+
+        public int Max { get; private set; }
+
+        /*
+        Using properties is like:
+        private int MAX = 50;
+        public int GetMax() {
+            return this.MAX
+        }
+
+        private void SetMax(int new_max) {
+            this.MAX = new_max;
+        }
+         */
+
+        public Integer()
+        {
+            Max = 50;
+        }
+
+        public int GetFirst()
+        {
+            return 0;
+        }
+
+        public int GetNext(int current)
+        {
+            return current + 1;
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            if (how_many > Max)
+            {
+                throw new Exception();
+            }
+            int[] seq_array = new int[how_many];
+            /*
+            for (int i = 0; i < how_many; i++)
+            {
+                seq_array[i] = i;
+            }*/
+            int counter = 0;
+            seq_array[counter] = GetFirst();
+            while (counter < how_many -1)
+            {
+                /* Attempt 1
+                seq_array[counter+1] = GetNext(seq_array[counter]);
+                counter++;
+                */
+                counter++;
+                seq_array[counter] = GetNext(seq_array[counter-1]);
+            }
+
+            return seq_array;
+        }
+    }
+}
