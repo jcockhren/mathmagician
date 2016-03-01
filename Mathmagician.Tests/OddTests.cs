c19d46bd9810e67b6d87d3bc14835544685a541e
Jurnell Cockhren <jurnell@sophicware.com>
2016-02-29 17:23:44 -0800

    Implmented Odd class with tests

---
+++ Mathmagician.Tests/OddTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class OddTests
+    {
+        [TestMethod]
+        public void OddEnsureICanCreateInstance()
+        {
+            Odd my_odds = new Odd();
+            Assert.IsNotNull(my_odds);
+        }
+
+        [TestMethod]
+        public void OddEnsureICanGetFirst()
+        {
+            // Arrange
+            Odd my_odds = new Odd();
+
+            // Act
+            int actual = my_odds.GetFirst();
+            int expected = 1;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OddEnsureICanGetNext()
+        {
+            // Arrange
+            Odd my_odds = new Odd();
+
+            // Act
+            int actual = my_odds.GetNext(5);
+            int expected = 7;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OddIntegerEnsureICanCreateASequenceOfTen()
+        {
+            // Arrange
+            Odd my_odds = new Odd();
+
+            // Act
+            int[] actual = my_odds.GetSequence(10);
+            int[] expected = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Mathmagician/Odd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Odd : Integer
+    {
+        public Odd()
+        {
+            First = 1;
+            Step = 2;
+        }
+    }
+}
