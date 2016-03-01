4509ac5ef5bbedd68586c6cc8c436f577551ad8f
Jurnell Cockhren <jurnell@sophicware.com>
2016-02-29 19:40:51 -0800

    Fibonacci Tests and implemmentation. Attempting to generate the sequence of Fibonacci during contruction in order to leverage GetNext without changes. Given GetNth is recursive, it will take forever. Let's find a middle ground for this.

---
+++ Mathmagician.Tests/FibonacciTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void FibonacciEnsureICanCreateInstance()
+        {
+            Fibonacci my_Fibonaccis = new Fibonacci();
+            Assert.IsNotNull(my_Fibonaccis);
+        }
+
+        [TestMethod]
+        public void FibonacciEnsureICanGetFirst()
+        {
+            // Arrange
+            Fibonacci my_Fibonaccis = new Fibonacci();
+
+            // Act
+            int actual = my_Fibonaccis.GetFirst();
+            int expected = 1;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciEnsureICanGetNext()
+        {
+            // Arrange
+            Fibonacci my_Fibonaccis = new Fibonacci();
+
+            // Act
+            int actual = my_Fibonaccis.GetNext(8);
+            int expected = 13;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciIntegerEnsureICanCreateASequenceOfTen()
+        {
+            // Arrange
+            Fibonacci my_Fibonaccis = new Fibonacci();
+
+            // Act
+            int[] actual = my_Fibonaccis.GetSequence(10);
+            int[] expected = new int[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciCalc10thFibonacciNumber()
+        {
+            // Arrange
+            Fibonacci my_Fibonaccis = new Fibonacci();
+
+            // Act
+            int actual = my_Fibonaccis.GetNth(10);
+            int expected = 55;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciCalc2ndFibonacciNumber()
+        {
+            // Arrange
+            Fibonacci my_Fibonaccis = new Fibonacci();
+
+            // Act
+            int actual = my_Fibonaccis.GetNth(2);
+            int expected = 1;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void FibonacciEnsureValidNs()
+        {
+            // Arrange 
+            Fibonacci my_fibo = new Fibonacci();
+
+            // Act
+            my_fibo.GetNth(-2);
+        }
+    }
+}
---
+++ Mathmagician/Fibonacci.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Fibonacci : Integer
+    {
+        private int[] elements; 
+        public Fibonacci()
+        {
+            First = 1;
+            elements = new int[Max];
+            for (int i = 0; i < Max; i++)
+            {
+                elements[i] = GetNth(i + 1);
+            }
+        }
+
+        public override int GetNext(int current)
+        {
+            return GetNth(current + 1);
+        }
+
+        public int GetNth(int n)
+        {
+            if (n <= 0) { throw new Exception(); }
+
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            } else
+            {
+                return GetNth(n-1) + GetNth(n-2);
+            }
+        }
+
+        override public int[] GetSequence(int how_many)
+        {
+            if (how_many > Max)
+            {
+                throw new Exception();
+            }
+            int[] seq_array = new int[how_many];
+            /*
+            int counter = 0;
+            seq_array[counter] = GetFirst();
+            while (counter < how_many - 1)
+            {
+                counter++;
+                seq_array[counter] = GetNext(seq_array[counter - 1]);
+            }
+            */
+
+            for (int n = 0; n < how_many; n++)
+            {
+                seq_array[n] = GetNth(n + 1);
+            }
+
+
+            return seq_array;
+        }
+    }
+}
