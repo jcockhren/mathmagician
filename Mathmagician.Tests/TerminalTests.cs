81b9c7794181704479b7bd7574f5aa4586b88de8
Jurnell Cockhren <jurnell@sophicware.com>
2016-03-02 19:33:23 -0800

    Basic tests and starting implementation for Terminal Class handling user input and output

---
+++ Mathmagician.Tests/TerminalTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class TerminalTests
+    {
+        [TestMethod]
+        public void TerminalEnsureICanCreateInstance()
+        {
+            Terminal term = new Terminal();
+
+            Assert.IsNotNull(term);
+        }
+
+        [TestMethod]
+        public void TerminalEnsureFindCorrectNumberFamily()
+        {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act
+            string actual_output = term.AskFirstQuestion();
+            string expected_output = "What would you like for me to do?";
+
+            // Assert
+            Assert.AreEqual(expected_output, actual_output);
+        }
+
+        [TestMethod]
+        public void TerminalProgressStartsAtNegative()
+        {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act
+            int actual = term.Progress;
+            int expected = -1;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TerminalFirstQuestionBadAnswer()
+        {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act
+            string user_input = "car";
+            string actual_output = term.AcceptFirstAnswer(user_input);
+            string expected_output = "I don't know what you're talking about...";
+
+            // Assert
+            Assert.AreEqual(expected_output, actual_output);
+            Assert.AreEqual(-1, term.Progress);
+        }
+
+        [TestMethod]
+        public void TerminalUserNeedsHelp()
+        {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act
+            string actual_output = term.PrintHelp();
+            string expected_output = "I only understand: Even, Prime, Fibonacci, Odd and Integer";
+
+            // Assert
+            Assert.AreEqual(expected_output, actual_output);
+        }
+    }
+}
---
+++ Mathmagician/Terminal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Terminal
+    {
+        string[] steps = { "Q1", "Q2", "ACK", "Ans" };
+
+        public int Progress { get; private set; }
+
+        public Terminal()
+        {
+            Progress = -1;
+        }
+
+        public string AskFirstQuestion()
+        {
+            return "What would you like for me to do?";
+        }
+
+        public string PrintHelp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AcceptFirstAnswer(string user_input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
