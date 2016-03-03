ac9360ba968c08435330baead7c45615a7d58cd3
Jurnell Cockhren <jurnell@sophicware.com>
2016-03-02 17:33:03 -0800

    Four tests that help use fix recursion bug that prevents calculcating lare values of 'n'.
    Array* Tests:
      1. int[] a = new int[10] creates an array of size 10 with all zeros.
      2. Teaches us how Array.FindIndex works.
    
    GetNthCached Tests show us how to short-circuit recursive call trees by filling in an array with already calcuclated answers.

--- Mathmagician.Tests/FibonacciTests.cs
+++ Mathmagician.Tests/FibonacciTests.cs
@@ -93,5 +93,68 @@ namespace Mathmagician.Tests
             // Act
             my_fibo.GetNth(-2);
         }
+
+        [TestMethod]
+        public void FibonacciCalc4thFiboWithCache()
+        {
+            // Arrange
+            Fibonacci my_fibo = new Fibonacci();
+
+            // Act
+            int actual = my_fibo.GetNthCached(4);
+            int expected = 3;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FibonacciCalc30thFiboWithCache()
+        {
+            // Arrange
+            Fibonacci my_fibo = new Fibonacci();
+
+            // Act
+            int actual = my_fibo.GetNthCached(30);
+            int expected = 832040;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ArraysAreTheItemsThere()
+        {
+            int[] elements = new int[10];
+            elements[0] = 2;
+            elements[1] = 4;
+            
+            int actual = elements[2];
+            int expected;
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void ArrayHowDoIUseFindIndex()
+        {
+            int[] my_sequence = new int[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+
+            int actual_index = Array.FindIndex(my_sequence, value => 8 == value);
+            int expected_index = 5;
+
+            Assert.AreEqual(expected_index, actual_index);
+        }
+
+        public static bool GetIt8(int value)
+        {
+            if (value == 8)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
     }
 }
