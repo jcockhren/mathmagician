ce3e157474e01e458d3bbd50f45f7c48bd1eda10
Jurnell Cockhren <jurnell@sophicware.com>
2016-03-07 19:38:46 -0800

    Implement Main method

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -10,6 +10,31 @@ namespace Mathmagician
     {
         static void Main(string[] args)
         {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act
+            Console.WriteLine(term.AskFirstQuestion());
+            string class_type = Console.ReadLine();
+            string first_ans = term.AcceptFirstAnswer(class_type);
+            if (term.Progress == 0)
+            {
+                Console.WriteLine(first_ans);
+                return;
+            } else
+            {
+                Console.WriteLine(first_ans);
+            }
+            string how_many = Console.ReadLine();
+            string second_ans = term.AcceptSecondAnswer(how_many);
+            if (term.Progress == 1)
+            {
+                Console.WriteLine(second_ans);
+                return;
+            }
+            string final_ans = term.PrintSequence();
+            Console.WriteLine(final_ans);
+            Console.ReadKey();
         }
     }
 }
