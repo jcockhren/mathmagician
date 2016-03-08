a1d0964a4a1a7a081e6303acdd1c557f0ca8bd47
Jurnell Cockhren <jurnell@sophicware.com>
2016-03-07 19:36:55 -0800

    Happy and Sad paths tests for terminal instance

--- Mathmagician.Tests/TerminalTests.cs
+++ Mathmagician.Tests/TerminalTests.cs
@@ -51,11 +51,11 @@ namespace Mathmagician.Tests
             // Act
             string user_input = "car";
             string actual_output = term.AcceptFirstAnswer(user_input);
-            string expected_output = "I don't know what you're talking about...";
+            string expected_output = "Whoops!";
 
             // Assert
             Assert.AreEqual(expected_output, actual_output);
-            Assert.AreEqual(-1, term.Progress);
+            Assert.AreEqual(0, term.Progress);
         }
 
         [TestMethod]
@@ -71,6 +71,7 @@ namespace Mathmagician.Tests
 
             // Assert
             Assert.AreEqual(expected_output, actual_output);
+            Assert.AreEqual(user_input, term.NumberFamily);
             Assert.AreEqual(1, term.Progress);
 
         }
@@ -105,6 +106,7 @@ namespace Mathmagician.Tests
 
             // Assert
             Assert.IsTrue(actual_output.StartsWith(expected_output));
+            Assert.AreEqual(10, term.Count);
             Assert.AreEqual(2, term.Progress);
 
         }
@@ -118,6 +120,104 @@ namespace Mathmagician.Tests
             // Act
             string actual_output = term.PrintHelp();
             string expected_output = "I only understand: Even, Prime, Fibonacci, Odd and Integer";
+
+            // Assert
+            Assert.AreEqual(expected_output, actual_output);
+        }
+
+        [TestMethod]
+        public void TerminalHappyPathPrimeNumbers()
+        {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act
+            term.AskFirstQuestion();
+            string first_input = "prime";
+            term.AcceptFirstAnswer(first_input); /* Create Prime class instance */
+            string second_input = "20";
+            string second_output = term.AcceptSecondAnswer(second_input);
+            string expected_second_output = "Cool. I'm going to print 20 prime numbers";
+
+            // Assert
+            Assert.AreEqual(expected_second_output, second_output);
+            Assert.AreEqual(2, term.Progress);
+            Assert.AreEqual(first_input,term.NumberFamily);
+            Assert.AreEqual(20, term.Count);
+
+        }
+
+        [TestMethod]
+        public void TerminalFirstQuestionSadPathPrimeNumbers()
+        {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act
+            term.AskFirstQuestion();
+            string first_input = "car";
+            string actual_output = term.AcceptFirstAnswer(first_input);
+            string expected_output = "Whoops!";
+
+            // Assert
+            Assert.AreEqual(expected_output, actual_output);
+            Assert.AreEqual(0, term.Progress);
+            Assert.IsNull(term.NumberFamily);
+            Assert.AreEqual(0, term.Count);
+
+        }
+
+        [TestMethod]
+        public void TerminalSecondQuestionSadPathPrimeNumbers()
+        {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act
+            term.AskFirstQuestion();
+            string first_input = "prime";
+            term.AcceptFirstAnswer(first_input);
+            string second_input = "2000";
+            string second_output = term.AcceptSecondAnswer(second_input);
+            string expected_second_output = "Whoops!";
+
+            // Assert
+            Assert.AreEqual(expected_second_output, second_output);
+            Assert.AreEqual(1, term.Progress);
+            Assert.AreEqual(first_input, term.NumberFamily);
+            Assert.AreEqual(0,term.Count);
+
+        }
+
+        [TestMethod]
+        public void TerminalInstantiatePrimeInstance()
+        {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act 
+            term.AskFirstQuestion();
+            string first_input = "prime";
+            term.AcceptFirstAnswer(first_input);
+            Integer actual_output = term.NumberFamilyFactory();
+
+            // Assert
+            Assert.IsInstanceOfType(actual_output, typeof(Prime));
+        }
+
+        [TestMethod]
+        public void TerminalPrintSequenceOfNumbers()
+        {
+            // Arrange
+            Terminal term = new Terminal();
+
+            // Act
+            string class_type = "integer";
+            term.AcceptFirstAnswer(class_type); /* Create Prime class instance */
+            string how_many = "10";
+            string second_output = term.AcceptSecondAnswer(how_many);
+            string actual_output = term.PrintSequence();
+            string expected_output = "0 1 2 3 4 5 6 7 8 9";
 
             // Assert
             Assert.AreEqual(expected_output, actual_output);
